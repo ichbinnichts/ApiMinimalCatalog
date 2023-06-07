44beafa33151dd821afdcaefc3c08eb162a472cf
Nathan Faria <nathanfaria607@gmail.com>
2023-06-07 14:39:42 -0300

    feat: add AppDbContext class

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiMinimalCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMinimalCatalog.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
